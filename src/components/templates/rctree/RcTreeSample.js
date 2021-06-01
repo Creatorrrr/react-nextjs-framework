c7ffde0199b8630d6f12ed32c702c1092c25bd8d
Soik Cha <ckthdlr@naver.com>
2021-06-01 12:56:26 +0900

    tree icon

--- src/components/templates/rctree/RcTreeSample.js
+++ src/components/templates/rctree/RcTreeSample.js
@@ -1,4 +1,6 @@
 import Tree from "rc-tree";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder, faUserFriends } from "@fortawesome/free-solid-svg-icons";
 import "rc-tree/assets/index.css";
 
 export default function RcTreeSample({ nodes }) {
@@ -6,13 +8,20 @@ export default function RcTreeSample({ nodes }) {
     return currentNodes.map((node) => ({
       key: node.id,
       title: node.text,
+      node: node.data,
       children: drawTreeItem(nodes.filter((childNode) => childNode.parent === node.id)),
     }));
   };
 
   return (
     <div style={{ margin: "0 20px" }}>
-      <Tree treeData={drawTreeItem(nodes.filter((childNode) => childNode.parent === "#"))} showLine checkable defaultExpandAll />
+      <Tree
+        treeData={drawTreeItem(nodes.filter((childNode) => childNode.parent === "#"))}
+        icon={({ data }) => (data.node.nodeTypeCode === "GROUP" ? <FontAwesomeIcon icon={faUserFriends} /> : <FontAwesomeIcon icon={faFolder} />)}
+        showLine
+        checkable
+        defaultExpandAll
+      />
     </div>
   );
 }
